99e19be1cf219b1e4088daa69d4b01d9d980664f
Manju KC <kcmanjuthapa@gmail.com>
2019-10-09 17:51:37 -0400

    added removeFeature function and pass the props to AddedFeatures

--- src/App.js
+++ src/App.js
@@ -9,21 +9,26 @@ import { addFeature, removeFeature } from "./actions";
 const App = props => {
   const removeFeature = item => {
     // dispatch an action here to remove an item
+    console.log(item);
+    removeItem(item);
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
-    props.addFeature(item)
+    props.addFeature(item);
   };
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures car={props.car} />
+        <AddedFeatures car={props.car}  removeFeature={removeFeature}/>
       </div>
       <div className="box">
-        <AdditionalFeatures store={props.additionalFeatures} buyItem={buyItem} />
+        <AdditionalFeatures
+          store={props.additionalFeatures}
+          buyItem={buyItem}
+        />
         <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
@@ -40,5 +45,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  {addFeature, removeFeature}
+  { addFeature, removeFeature }
 )(App);
